67ae0ea9c547b2be0d383ecda64248c22be91e6a
merkader69 <calibur99@hotmail.com>
2022-04-27 22:40:46 -0400

    endpoints

---
+++ endpoints.py
@@ -0,0 +1,31 @@
+import json
+
+
+class Endpoints:
+    def __init__(self):
+        self.url = "https://api.nal.usda.gov/fdc/v1/foods/"
+
+    def url(self):
+        return self.url
+
+    def end_list(self, api_key, page_size=200, page_number=1, sort_by="lowercaseDescription.keyword", sort_order="desc"):
+        if api_key is None:
+            return "API Key required."
+        payload = {
+            "api_key": api_key, page_size: "page_size", "page_number": page_number,
+            "sort_by": sort_by, "sort_order": sort_order}
+        return f"{self.url}list", payload
+
+    def end_search(self, api_key, query=None, description=None):
+        if description is None:
+            description = ["Branded", "SR Legacy"]
+        if api_key is None:
+            return "API Key required."
+        payload = {
+            "api_key": api_key, "query": query, "description": description
+        }
+        return f"{self.url}list", payload
+
+
+if __name__ == '__main__':
+    print(Endpoints().end_list(api_key="1"))
