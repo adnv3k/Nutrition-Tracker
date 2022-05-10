922b8bd93db8ca909e81c1e872d0bd212139152e
merkader69 <calibur99@hotmail.com>
2022-05-09 22:49:27 -0400

    made progress on improving the search query filter, but need to test. may run into issues since the filter now checks if the query starts and ends with a certain letter and compares it to the DB. also now ranks the entries automatically using SearchRank(), but need to implement weights.

--- food_track/foods/views.py
+++ food_track/foods/views.py
@@ -8,7 +8,7 @@ from django.views.generic import TemplateView, ListView, View
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import Paginator
 from django.utils import timezone
-from django.contrib.postgres.search import SearchVector, SearchQuery
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.contrib import messages
 
 from .models import Food, FoodHistory
@@ -32,8 +32,17 @@ class SearchResultsView(ListView):
 
         params['dataType'] = data_type
 
-        food_query = requests.get(url, params=params)
-        if len(food_query.json()) < 3:
+        start = time.time()
+        while True:
+            try:
+                food_query = requests.get(url, params=params, timeout=30)
+                break
+            except ConnectionError:
+                if time.time() > start + 30:
+                    raise Exception('Unable to reach USDA API after 30 seconds of connection errors.')
+                else:
+                    time.sleep(1)
+        if not len(food_query.json()) > 0:
             return 'None'
         food_l = []
         for food in food_query.json():
@@ -48,18 +57,12 @@ class SearchResultsView(ListView):
 
             # if not Food.objects.filter(name=food_dict['food']['description']).exists():
             #    Food.objects.get_or_create(name=food_dict['food']['description'], nutrients=nutrients_clean)
-            try:
-                Food.objects.get(name=food['description'], dataType=params['dataType'])
-            except ObjectDoesNotExist:
-                Food.objects.get_or_create(
-                    name=food['description'], nutrients=nutrients_clean, dataType=params['dataType'])
-            else:
-                pass
-
-            p = Paginator(Food.objects.all().filter(Q(name__icontains=[query])).first(), 25)
-            page_num = self.request.GET.get('page')
-            page_obj = p.get_page(page_num)
-            return render(self.request, 'search_results.html', {'page_obj': page_obj})
+            Food.objects.get_or_create(
+                name=food['description'], nutrients=nutrients_clean, dataType=params['dataType'])
+        p = Paginator(Food.objects.all().filter(Q(name__icontains=[query])), 25)
+        page_num = self.request.GET.get('page')
+        page_obj = p.get_page(page_num)
+        return render(self.request, 'search_results.html', {'page_obj': page_obj})
 
     def get_queryset(self):
         q = self.request.GET.get("q")
@@ -68,8 +71,11 @@ class SearchResultsView(ListView):
         else:
             dataType = 'SR Legacy'
 
+        vector = SearchVector("name", "dataType")
+        query = SearchQuery(q, search_type='phrase') & SearchQuery(dataType, search_type='phrase')
         food_q = Food.objects.annotate(
-            search=SearchVector("name", "dataType")).filter(search=SearchQuery(q) & SearchQuery(dataType))
+            rank=SearchRank(vector, query), search=vector) \
+            .filter(search=query, name__istartswith=q[0], name__iendswith=q[-1]).order_by('-rank')
         if food_q.exists():
             return food_q
         else:
