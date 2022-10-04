1f13ffd7fe74af9a5c239d91a9bfb65f2a08a480
adnv3k <adnv3k@gmail.com>
2022-10-04 00:48:55 -0700

    update commands

--- config/client.py
+++ config/client.py
@@ -1,8 +1,6 @@
 import subprocess
 
-commands = ["sudo apt-get -y install postgresql-12",
-            "sudo service postgresql-12 initdb",
-            "sudo service postgresql-12 start"]
+commands = ["sudo service postgresql start"]
 for command in commands:
     process = subprocess.Popen(command.split(),stdout=subprocess.PIPE)
     output, error = process.communicate()
